5d0ea2577f319b65e8e1ffc69d80216ca39b7bb1
Norikazu Saito <50255317+norikazu-saito@users.noreply.github.com>
2026-05-14 14:11:15 +0900

    first commit

---
+++ fem/header2.py
@@ -0,0 +1,9 @@
+# packages
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.tri as mtri
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+# Mounting Google Drive
+from google.colab import drive
+drive.mount('/content/drive')
